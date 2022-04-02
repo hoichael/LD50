12a710df5a338605c47d2f85cf8d9432367573a9
hoichael <31223078+hoichael@users.noreply.github.com>
2022-04-02 17:13:08 +0200

    Create simple UI reticle + create flashlight activate anim

--- u_LD50/Assets/Scripts/Player/Dev/pl_D_flashlight.cs
+++ u_LD50/Assets/Scripts/Player/Dev/pl_D_flashlight.cs
@@ -7,11 +7,27 @@ public class pl_D_flashlight : MonoBehaviour
     [SerializeField]
     private Light flashlight;
 
+    [SerializeField]
+    private float xRotHide;
+
+    private float xRotCurrent;
+
+    [SerializeField]
+    private float hideAnimLerpFactor;
+
+    private float currentLerpTarget;
+
     private bool currentlyEnabled;
 
     private void Start()
     {
         currentlyEnabled = flashlight.enabled;
+
+        if(!currentlyEnabled)
+        {
+            xRotCurrent = currentLerpTarget = xRotHide;
+            transform.localRotation = Quaternion.Euler(xRotCurrent, 0, 0);
+        }
     }
 
     private void Update()
@@ -20,11 +36,28 @@ public class pl_D_flashlight : MonoBehaviour
         {
             ToggleFlashlight();
         }
+
+        if (transform.localRotation.x == currentLerpTarget) return;
+
+        HandleToggleAnim();
     }
 
     private void ToggleFlashlight()
     {
+        currentLerpTarget = currentlyEnabled ? xRotHide : 0;
+
         currentlyEnabled = !currentlyEnabled;
         flashlight.enabled = currentlyEnabled;
+    }
+
+    private void HandleToggleAnim()
+    {
+        xRotCurrent = Mathf.Lerp(
+            xRotCurrent,
+            currentLerpTarget,
+            hideAnimLerpFactor * Time.deltaTime
+            );
+
+        transform.localRotation = Quaternion.Euler(xRotCurrent, 0, 0);
     }
 }
