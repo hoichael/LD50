f1bf41e6b32c86e0eb5e1bef11d1dbb54e589c14
hoichael <31223078+hoichael@users.noreply.github.com>
2022-04-08 23:04:47 +0200

    Implement box activation animation

--- u_LD50/Assets/Scripts/Player/Camera/pl_cam_rot.cs
+++ u_LD50/Assets/Scripts/Player/Camera/pl_cam_rot.cs
@@ -16,48 +16,104 @@ public class pl_cam_rot : MonoBehaviour
     [SerializeField]
     private Transform toolsContainer;
 
-    private float rotX, rotY;
+    public float rotX, rotY;
 
-    public bool lookAtBox;
-    public bool resetBoxLook;
+    [SerializeField]
+    private pl_box boxScript;
+
+    private Vector3 initRot;
+
+    [SerializeField]
+    private Vector3 boxActiveRot;
+
+    private bool transitionActive;
+    private bool boxActive;
+
+    [SerializeField]
+    private Vector3 camHolderPosDefault;
+
+    [SerializeField]
+    private Vector3 camHolderPoxBox;
+
 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        transform.localPosition = camHolderPosDefault;
+    }
+
+    public void InitBox()
+    {
+        initRot = transform.localRotation.eulerAngles;
+        if (initRot.x > 90)
+        {
+            initRot.x = 0 - (360 - initRot.x);
+        }
+
+
+        boxActiveRot.y = transform.localRotation.eulerAngles.y;
+        print(initRot);
+        transitionActive = true;
+    }
+
+    public void InitCloseBox()
+    {
+        transitionActive = true;
     }
 
     private void Update()
     {
-        if(lookAtBox)
+        if(transitionActive)
         {
-            LookAtBox();
-        }
-        else if(resetBoxLook)
-        {
-            ResetBoxLook();
+            HandleBoxTransition();
         }
         else
         {
             HandleInput();
+            ApplyRotation();
         }
-        ApplyRotation();
     }
 
-    private void LookAtBox()
+    private void HandleBoxTransition()
     {
-        rotX = 90;
-    }
+        transform.localRotation = Quaternion.Lerp(
+            Quaternion.Euler(initRot),
+            Quaternion.Euler(boxActiveRot),
+            boxScript.transitionProgress);
 
-    private void ResetBoxLook()
-    {
-        rotX = 0;
-        lookAtBox = false;
-        resetBoxLook = false;
+        transform.localPosition = Vector3.Lerp(
+            camHolderPosDefault,
+            camHolderPoxBox,
+            boxScript.transitionProgress);
+
+        if(boxScript.transitionProgress == boxScript.currentTransitionTarget)
+        {
+            transitionActive = false;
+
+            rotX = transform.localRotation.eulerAngles.x;
+            if (rotX > 90)
+            {
+                rotX = 0 - (360 - rotX);
+            }
+
+            rotY = transform.localRotation.eulerAngles.y;
+
+            if(boxScript.currentTransitionTarget == 1)
+            {
+                boxActive = true;
+            }
+            else
+            {
+                boxActive = false;
+            }
+        }
     }
 
     private void HandleInput()
     {
+        if (boxActive) return;
+
         rotY += input.mouseX * pl_settings.Instance.mouseSens;
         rotX -= input.mouseY * pl_settings.Instance.mouseSens;
 
--- u_LD50/Assets/Scripts/Player/General/pl_box.cs
+++ u_LD50/Assets/Scripts/Player/General/pl_box.cs
@@ -11,49 +11,118 @@ public class pl_box : MonoBehaviour
     private Rigidbody rb;
 
     [SerializeField]
-    private Transform anchor;
+    private Transform anchorBox;
 
     [SerializeField]
-    private GameObject container;
+    private Transform anchorLid;
+
+    [SerializeField]
+    private GameObject containerObj;
 
     [SerializeField]
     private pl_cam_rot camRot;
+
+    [SerializeField]
+    private Vector3 anchorBoxActivePos;
+
+    [SerializeField]
+    private Vector3 anchorBoxHiddenPos;
+
+    [SerializeField]
+    private Vector3 anchorLidHiddenRot;
+
+    [SerializeField]
+    private Vector3 anchorLidActiveRot;
 
     private List<GameObject> currentObjects;
 
     private bool currentlyActive;
 
+    [SerializeField]
+    private float transitionSpeed;
+
+    public float transitionProgress;
+    public float currentTransitionTarget;
+    private bool transitionActive;
+
+    private void Start()
+    {
+        anchorBox.localPosition = anchorBoxHiddenPos;
+        anchorLid.localRotation = Quaternion.Euler(anchorLidHiddenRot);
+        containerObj.SetActive(false);
+    }
+
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E) && pl_state.Instance.grounded)
+        if(Input.GetKeyDown(KeyCode.E) && pl_state.Instance.grounded && !transitionActive)
         {
+            transitionActive = true;
             if(currentlyActive)
             {
-                CloseBox();
+                DeactivateBox();
             }
             else
             {
-                OpenBox();
+                ActivateBox();
+            }
+        }
+
+        if (!transitionActive) return;
+        HandleTransitionAnim();
+    }
+
+    private void HandleTransitionAnim()
+    {
+        transitionProgress = Mathf.MoveTowards(transitionProgress, currentTransitionTarget, transitionSpeed * Time.deltaTime);
+
+        anchorBox.localPosition = Vector3.Lerp(
+            anchorBoxHiddenPos, 
+            anchorBoxActivePos, 
+            transitionProgress);
+
+        anchorLid.localRotation = Quaternion.Lerp(
+            Quaternion.Euler(anchorLidHiddenRot), 
+            Quaternion.Euler(anchorLidActiveRot), 
+            transitionProgress);
+
+        if (transitionProgress == currentTransitionTarget)
+        {
+            transitionActive = false;
+            if(currentTransitionTarget == 0)
+            {
+                containerObj.SetActive(false);
             }
         }
     }
 
-    private void OpenBox()
+
+    private void ActivateBox()
     {
         move.currentMult = 0;
-        camRot.lookAtBox = true;
+
         rb.velocity = Vector3.zero;
+        rb.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
+        rb.isKinematic = true;
+
+        currentTransitionTarget = 1;
+
+        containerObj.SetActive(true);
         currentlyActive = true;
-    //    rb.isKinematic = true;
+
+        camRot.InitBox();
     }
 
-    private void CloseBox()
+    private void DeactivateBox()
     {
         move.currentMult = 1;
-        camRot.lookAtBox = false;
-        camRot.resetBoxLook = true;
-        currentlyActive = false;
-        //   rb.isKinematic = false;
-    }
 
+        rb.isKinematic = false;
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+
+        currentTransitionTarget = 0;
+
+        currentlyActive = false;
+
+        camRot.InitCloseBox();
+    }
 }
