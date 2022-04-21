62be56607c2201e8cd2fc4c717eec763f449dffe
hoichael <31223078+hoichael@users.noreply.github.com>
2022-04-21 20:26:56 +0200

    Implement en_swiper dmg and health + move some files

--- u_LD50/Assets/Scripts/Enemies/Types/Swiper/States/en_swiper_attack.cs
+++ u_LD50/Assets/Scripts/Enemies/Types/Swiper/States/en_swiper_attack.cs
@@ -7,6 +7,18 @@ public class en_swiper_attack : en_state_base
     [SerializeField]
     private float duration;
 
+    [SerializeField]
+    private Transform attackPosTrans;
+
+    [SerializeField]
+    private Vector3 attackColHalfExtents;
+
+    [SerializeField]
+    private LayerMask playerLayerMask;
+
+    [SerializeField]
+    private dmg_base dmgInfo;
+
     protected override void OnEnable()
     {
         base.OnEnable();
@@ -18,9 +30,28 @@ public class en_swiper_attack : en_state_base
 
     private IEnumerator HandleDuration()
     {
-        yield return new WaitForSeconds(duration);
+        yield return new WaitForSeconds(duration * 0.5f);
+
+        HandleHitbox();
+
+        yield return new WaitForSeconds(duration * 0.5f);
 
         ChangeState("chase");
+    }
+
+    private void HandleHitbox()
+    {
+        Collider[] hitColliders = Physics.OverlapBox(attackPosTrans.position, attackColHalfExtents, Quaternion.identity, playerLayerMask);
+        if (hitColliders.Length > 0)
+        {
+            for (int i = 0; i < hitColliders.Length; i++)
+            {
+                if (hitColliders[i].isTrigger == true)
+                {
+                    hitColliders[i].GetComponent<pl_health_damage>().HandleDamage(dmgInfo);
+                }
+            }
+        }
     }
 
     protected override void OnDisable()
---
+++ u_LD50/Assets/Scripts/Enemies/Types/Swiper/en_swiper_health.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class en_swiper_health : en_health_base
+{
+    [SerializeField]
+    private en_ragdoll ragdoll;
+
+    [SerializeField]
+    private GameObject states;
+
+    [SerializeField]
+    private en_brain_base brain;
+    protected override void HandleDeath(dmg_base dmgInfo)
+    {
+        base.HandleDeath(dmgInfo);
+        transform.tag = "Corpse";
+
+        //   brain.currentState.enabled = false;
+        //   brain.enabled = false;
+        states.SetActive(false);
+
+        ragdoll.ToggleRagdoll(true);
+
+        ragdoll.rbRagRoot.AddForce(Vector3.up * 60f, ForceMode.Impulse);
+
+        ragdoll.rbRagRoot.AddForce((transform.position - pl_state.Instance.GLOBAL_CAM_REF.transform.position).normalized * 95f, ForceMode.Impulse);
+    }
+}
