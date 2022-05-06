6b246458369fe43fe9889e46a41ae9859eaead54
hoichael <31223078+hoichael@users.noreply.github.com>
2022-05-06 03:54:14 +0200

    Setup ed_menu_gen custom editor tool

---
+++ u_LD50/Assets/Editor/ed_menu_gen.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ed_menu_gen : EditorWindow
+{
+    private GameObject buttonContainer;
+    private GameObject charTMPbase;
+    private string text;
+    private char[] charArr;
+
+    private int charSpacing = 2;
+
+    [MenuItem("Custom/GenerateButton")]
+    public static void OpenWindow()
+    {
+        GetWindow(typeof(ed_menu_gen));
+    }
+
+    private void OnGUI()
+    {
+        buttonContainer = EditorGUILayout.ObjectField("Button Container", buttonContainer, typeof(GameObject), true) as GameObject;
+        GUILayout.Space(40);
+
+        charTMPbase = EditorGUILayout.ObjectField("TMP Base Prefab", charTMPbase, typeof(GameObject), false) as GameObject;
+        GUILayout.Space(40);
+
+        text = EditorGUILayout.TextField("Button Text", text);
+        GUILayout.Space(40);
+
+        if (GUILayout.Button("Execute"))
+        {
+            Init();
+        }
+    }
+
+    private void Init()
+    {
+        if (buttonContainer == null || text.Length == 0 || charTMPbase == null) return;
+
+        charArr = text.ToCharArray();
+
+        if (buttonContainer.transform.Find("CharacterContainer") != null)
+        {
+            DestroyImmediate(buttonContainer.transform.Find("CharacterContainer").gameObject);
+        }
+
+        var container = new GameObject();
+        container.name = "CharacterContainer";
+        container.transform.SetParent(buttonContainer.transform);
+
+        for(int i = 0; i < charArr.Length; i++)
+        {
+            var charHolder = new GameObject();
+            charHolder.name = "CharHolder";
+            charHolder.transform.SetParent(container.transform);
+
+            var tmpBase = Instantiate(charTMPbase);
+            tmpBase.transform.SetParent(charHolder.transform);
+
+            charHolder.transform.localPosition = new Vector3(i * charSpacing, 0, 0);
+
+            TMPro.TMP_Text textEl = tmpBase.GetComponent<TMPro.TMP_Text>();
+            textEl.text = charArr[i].ToString();
+        }
+    }
+}
